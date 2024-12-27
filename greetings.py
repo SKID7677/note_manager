6fce5a6187a18163ee34f289c5f41b03b58e8193
SKID7677 <sergey.kuznetsov7@list.ru>
2024-12-27 23:14:00 +0300

    g1

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'имя пользователя'
+title = 'заголовок заметки'
+content = 'описание заметки'
+status = 'статус заметки'
+created_date = '27-12-2024'
+issue_date = '31-12-2024'
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
